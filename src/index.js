50e6f5baca1115f5fd04d044335e94b0e4145a9f
Gerrit Lane <lane.gerrit@gmail.com>
2022-10-12 21:18:12 -0700

    Setup basic page and styles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<React.StrictMode><h1>Hello World!</h1></React.StrictMode>);
+root.render(<React.StrictMode><App /></React.StrictMode>);
