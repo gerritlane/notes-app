7f6a6de47e6c0eafcc6c723e1a4bfd603f8b191e
Gerrit Lane <lane.gerrit@gmail.com>
2022-10-13 20:25:49 -0700

    Add dynamic year to footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+const currentYear = new Date().getFullYear();
+
 function Footer () {
 	return (
 		<div className='footer'>
 			<div className='footer__text'>
-				web design by <a href="https://gerritlane.github.io/">@gerritlane</a>
+				web design by <a href="https://gerritlane.github.io/">@gerritlane</a> {currentYear}
 			</div>
 		</div>
 	);
