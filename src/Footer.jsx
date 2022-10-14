cda2d8485a0e2782d7206adcb4388b46227a8ae1
Gerrit Lane <lane.gerrit@gmail.com>
2022-10-13 20:11:58 -0700

    Add footer and adjust spacing

---
+++ src/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer () {
+	return (
+		<div className='footer'>
+			<div className='footer__text'>
+				web design by <a href="https://gerritlane.github.io/">@gerritlane</a>
+			</div>
+		</div>
+	);
+};
+
+export default Footer;
