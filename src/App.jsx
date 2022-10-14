55a41b8aa2fa54f3bf8033f167109eb989928fd7
Gerrit Lane <lane.gerrit@gmail.com>
2022-10-13 20:20:24 -0700

    Add note component and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import Note from './Note';
 
 function App() {
 	return (
 		<div className='outer'>
 			<Header />
+			<div className='note__container'>
+				<Note />
+				<Note />
+			</div>
 			<Footer />
 		</div>
 	);
---
+++ src/Note.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Note () {
+	return (
+		<div className='note'>
+			<div className='note__title'>This is a note title</div>
+			<div className='note__text'>This is the note text</div>
+		</div>
+	);
+};
+
+export default Note;
